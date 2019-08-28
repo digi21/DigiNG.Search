2bb2a0184d6d278b11d9c04cfd1329cb52fe8c0d
José Ángel Martínez Torres <joseangelmt@outlook.com>
2019-08-28 09:40:03 +0200

    Cambio en el número de versión

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.0.0.0")]
-[assembly: AssemblyFileVersion("2017.0.0.0")]
+[assembly: AssemblyVersion("2017.0.1.0")]
+[assembly: AssemblyFileVersion("2017.0.1.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
