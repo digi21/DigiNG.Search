222c895fb444d0b552465e918ba2f2e83c7a1138
José Ángel <joseangelmt@outlook.com>
2023-08-22 13:49:09 +0200

    Cambios para compilar en .NET 7.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// La información general sobre un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos atributos para modificar la información
-// asociada con un ensamblado.
-[assembly: AssemblyTitle("DigiNG.Search")]
-[assembly: AssemblyDescription("Implementa herramientas de búsqueda para Digi3D.NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dreaming With Objects S.L.")]
-[assembly: AssemblyProduct("Digi21.Search")]
-[assembly: AssemblyCopyright("Copyright © 2010-2022 Dreaming With Objects S.L.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,18 +11,4 @@ using System.Runtime.InteropServices;
 
 // El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
 [assembly: Guid("62ee782f-b833-4103-8ed1-8dae9a9bc5fb")]
-
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria 
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
-// mediante el asterisco ('*'), como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.3.0.0")]
-[assembly: AssemblyFileVersion("22.3.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
 
